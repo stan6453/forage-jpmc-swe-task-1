85d3f3fa1aa93c464d622975d3093ab1cf1134d9
Stanley Ezechukwu <stanley6453@gmail.com>
2023-10-23 13:59:52 +0100

    fixed wrong data calculations in clinet.py and wrote unit tests to prevent such errors

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,9 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    stock, bid_price, ask_price, price = getDataPoint(quotes[0])
+    self.assertEqual(price, 120.84)
+
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,9 +18,17 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    stock, bid_price, ask_price, price = getDataPoint(quotes[0])
+    self.assertEqual(price, 119.84)
 
 
   """ ------------ Add more unit tests ------------ """
+
+  def test_getRatio_returns_correct_value(self):
+    self.assertEqual(1.170514378290806, getRatio(144.5,123.45))
+
+  def test_getRatio_handles_division_by_zero(self):
+    self.assertIsNone(getRatio(144.5, 0))
 
 
 
